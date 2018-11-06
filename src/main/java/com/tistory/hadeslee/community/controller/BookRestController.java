a325e9e48e5c6a96ed0754e555d8f57faf5da498
sklee <hadesleeq@gmail.com>
2018-11-06 15:47:07 +0900

    REST 테스트용 BookRestService 생성

--- src/main/java/com/tistory/hadeslee/community/controller/BookRestController.java
+++ src/main/java/com/tistory/hadeslee/community/controller/BookRestController.java
@@ -1,10 +1,11 @@
 package com.tistory.hadeslee.community.controller;
 
+import com.tistory.hadeslee.community.domain.Book;
+import com.tistory.hadeslee.community.service.BookRestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
 
 
 @RestController
@@ -13,7 +14,7 @@ public class BookRestController {
     @Autowired
     private BookRestService bookRestService;
 
-    @GetMapping(path = "/rest/test",produces = PageAttributes.MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/rest/test", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book getTestBooks() {
         return bookRestService.getRestBook();
     }
