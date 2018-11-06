18183fad34142dbe45fb33c996ce5629b5f7a081
sklee <hadesleeq@gmail.com>
2018-11-06 13:44:07 +0900

    Fruit pojo매핑 테스트

--- src/main/java/com/tistory/hadeslee/community/pojo/Fruit.java
+++ src/main/java/com/tistory/hadeslee/community/pojo/Fruit.java
@@ -1,5 +1,8 @@
 package com.tistory.hadeslee.community.pojo;
 
+import lombok.Data;
+
+@Data
 public class Fruit {
     private String name;
     private String color;
--- src/test/java/com/tistory/hadeslee/community/pojo/FruitPropertyTest.java
+++ src/test/java/com/tistory/hadeslee/community/pojo/FruitPropertyTest.java
@@ -7,10 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.Map;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,16 +20,16 @@ public class FruitPropertyTest {
 
     @Test
     public void test() {
-        List<Map> fruitData = fruitProperty.getList();
+        List<Fruit> fruitData = fruitProperty.getList();
 
-        assertThat(fruitData.get(0).get("name"), is("banana"));
-        assertThat(fruitData.get(0).get("color"), is("yellow"));
+        assertThat(fruitData.get(0).getName(), is("banana"));
+        assertThat(fruitData.get(0).getColor(), is("yellow"));
 
-        assertThat(fruitData.get(1).get("name"), is("apple"));
-        assertThat(fruitData.get(1).get("color"), is("red"));
+        assertThat(fruitData.get(1).getName(), is("apple"));
+        assertThat(fruitData.get(1).getColor(), is("red"));
 
-        assertThat(fruitData.get(2).get("name"), is("water melon"));
-        assertThat(fruitData.get(2).get("color"), is("green"));
+        assertThat(fruitData.get(2).getName(), is("water melon"));
+        assertThat(fruitData.get(2).getColor(), is("green"));
 
 
     }
