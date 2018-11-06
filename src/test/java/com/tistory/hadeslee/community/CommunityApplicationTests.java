715e4a28382f11a69ed841f51ccc00e11e92fdc4
sklee <hadesleeq@gmail.com>
2018-11-06 14:14:32 +0900

    @SBootTest 파라미터 사용하기

--- src/test/java/com/tistory/hadeslee/community/CommunityApplicationTests.java
+++ src/test/java/com/tistory/hadeslee/community/CommunityApplicationTests.java
@@ -2,15 +2,25 @@ package com.tistory.hadeslee.community;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(value = "value=test", properties = {"property.value=propertyTest"},
+        classes = {CommunityApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CommunityApplicationTests {
+
+    @Value("${value")
+    private String value;
+
+    @Value("${property.value")
+    private String propertyValue;
 
     @Test
     public void contextLoads() {
+        //assertThat(value, is("test"));
+        //assertThat(propertyValue, is("propertyTest"));
     }
 
 }
