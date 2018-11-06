8d185ebb48dfb8311866fc21b34d19d9e9f4d694
sklee <hadesleeq@gmail.com>
2018-11-06 15:38:52 +0900

    @DataJpaTest로 테스트 수행하기

--- src/main/java/com/tistory/hadeslee/community/repository/BookRepository.java
+++ src/main/java/com/tistory/hadeslee/community/repository/BookRepository.java
@@ -3,5 +3,12 @@ package com.tistory.hadeslee.community.repository;
 import com.tistory.hadeslee.community.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findAll();
+
+    Book getOne(Integer idx);
+
+    void deleteAll();
 }
